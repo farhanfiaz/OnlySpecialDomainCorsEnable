1314b60248c10b11d493761768a4ed69c348c6db
farhanfiaz <59507472+farhanfiaz@users.noreply.github.com>
2022-10-10 17:43:12 +0500

    Mian Code Uploaded

---
+++ ClientTwo/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using ClientTwo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace ClientTwo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<dynamic> weatherList = new List<dynamic>();
+            try
+            {
+                
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync("https://localhost:7141/WeatherForecast"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        weatherList = JsonConvert.DeserializeObject<List<dynamic>>(apiResponse);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return View(weatherList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
